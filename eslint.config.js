cec187f07ac8df1d7d7a5e76c9650eb2c2cc7e8c
Guilherme. <guilherme.melo4631@gmail.com>
2026-08-10 22:00:53 -0300

    add: prettier tailwind plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from 'eslint-plugin-react-hooks'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +24,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      eslintConfigPrettier,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -29,10 +32,13 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
+  eslintConfigPrettier,
 ]
